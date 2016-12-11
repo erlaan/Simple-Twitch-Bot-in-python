284617831ec604b65d515bdc2d38d5ddce0bd9ce
Erlaan <jonathan@erlaan.se>
2016-12-11 20:10:37 +0100

    I made some cleaning

--- bot/bot.py
+++ bot/bot.py
@@ -3,8 +3,8 @@
 import socket
 
 #Some varibles!
-lab = "Har inga labbar just nu!"
-lab1 = "TOM"
+command = "I have no custom commands just now!"
+command1 = "Empty"
 
 # Some basic variables used to configure the bot
 server = "irc.twitch.tv" # Server
@@ -27,45 +27,28 @@ def hello(): # This function responds to a user that inputs "Hello Mybot"
 def tyst(): # This function responds to a user that he gona be quiet!
   ircsock.send("PRIVMSG "+ channel +" :TYST!\n")
 
-def mat(): # This function responds to a user wich food it is
-  ircsock.send("PRIVMSG "+ channel + " :Read here for att decide dig vart du ska eat! http://lunchguide.nu/hudiksvall \n")
-
-def matkebab(): #Fat kid!
-  ircsock.send("PRIVMSG "+ channel + " :FAT KID!! \n")
-
 def hailsandia(): # ALL HAIL SANDIA!
-  ircsock.send("PRIVMSG "+ "#etiskhackare-15"  + " : ALL HAIL SAND1A! \n")
+  ircsock.send("PRIVMSG "+ channel  + " : ALL HAIL SAND1A! \n")
 
-def jonte(msg):
-  ircsock.send("PRIVMSG "+ channel + " :" + ' '.join([x for x in msg if x != x.upper()]) + "\n" )
 
-def kick(user):
-  ircsock.send("KICK "+ "#etiskhackare-15"  + " : " + user + "\n")
-
-def msg(msg):
-  ircsock.send("PRIVMSG "+ channel + " :" + msg + "\n")
-
-def ladd(labb):
- global lab
- global lab1
- if lab1 == "TOM" :
-  lab1 = labb
+def cadd(cabb):
+ global command
+ global command1
+ if command1 == "Empty" :
+  command1 = cabb
  else :
-  lab1 = lab1 + " , "  + labb
+  command1 = command1 + " , "  + cabb
 
- lab = lab1
- #return lab
-def labs():
- ircsock.send("PRIVMSG " + channel + " :" + lab + "\n")
+ command = command1
+ #return command
+def commands():
+ ircsock.send("PRIVMSG " + channel + " :" + command + "\n")
 
 def ldel():
- global lab
- global lab1
- lab = "Har inga labbar just nu!"
- lab1 = "TOM"
-
-def welcom():
- ircsock.send("PRIVMSG " + channel + " :Hello and Welcome To EtiskHackare-15! \n")
+ global command
+ global command1
+ command = "I have no custom commands just now!"
+ command1 = "Empty"
 
 def rip():
  ircsock.send("PRIVMSG " + channel + " :Rest in PEACE! \n")
@@ -86,44 +69,25 @@ while 1: # Be careful with these! it might send you to an infinite loop
   if ircmsg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
     hello()
 
-  if ircmsg.lower().find(":hello ") != -1 :
+  if ircmsg.find(":hello ") != -1 :
     hello()
+
+  if ircmsg.find(":*!")
 
   if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
     ping()
 
-  if ircmsg.find(":!tyst") != -1:
-    tyst()
-
-  if ircmsg.find(":!mat kebab") != -1:
-    matkebab()
-
-  elif ircmsg.find(":!mat") != -1:
-    mat()
 
   if ircmsg.find(":!sandia") != -1:
     hailsandia()
 
-  if ircmsg.find(":!jonte") != -1:
-    jonte(ircmsg.split(":!jonte")[1])
+  if ircmsg.find(":!command") != -1:
+    commands()
+  if ircmsg.find(":!cadd") != -1:
+    cadd(ircmsg.split(":!cadd")[1])
 
-  if ircmsg.find(":!kick") != -1:
-    kick(ircmsg.split(":!kick")[1])
-
-  if ircmsg.find(":!send") != -1:
-    msg(ircmsg.split(":!send")[1])
-
-  if ircmsg.find(":!ladd") != -1:
-    ladd(ircmsg.split(":!ladd")[1])
-
-  elif ircmsg.find(":!ldel") != -1:
-    ldel()
-
-  elif ircmsg.find(":!labs") != -1:
-    labs()
-
- # if ircmsg.find("JOIN") != -1:
-  #  welcom()
+  elif ircmsg.find(":!cdel") != -1:
+    cdel()
 
   if ircmsg.find("QUIT") != -1:
     rip()
