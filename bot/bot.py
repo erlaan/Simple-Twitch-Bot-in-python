229e45ff67706692191bffd73d67d6e2792e0801
Erlaan <erlaan@localhost.localdomain>
2019-05-12 21:25:35 +0200

    I have added roll function and update hello function so the bot will say the username of the caller

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Import libraries.
-import socket
+import socket, random
 
 # Some basic variables used to configure the bot
 server = "irc.chat.twitch.tv" # Server
@@ -18,11 +18,22 @@ def sendmsg(chan , msg): # This is the send message function, it simply sends me
 def joinchan(chan): # This function is used to join channels.
   ircsock.send("JOIN ".encode()+ chan +"\n".encode())
 
-def hello(): # This function responds to a user that inputs "Hello Mybot"
-  ircsock.send("PRIVMSG ".encode()+ channel +" :Hello!\n".encode())
+def hello(msg): # This function responds to a user that inputs "Hello Mybot"
+  name = username(msg)
+  ircsock.send("PRIVMSG ".encode()+ channel +" :Hello ".encode() + name.encode() + "\n".encode())
 
 def help(): # This will respond what commands that exist for the bot!
     ircsock.send("PRIVMSG ".encode() + channel +" :I have no commands righ now!".encode())
+
+def roll(msg):
+  rand = random.randint(1,100)
+  randtostring = str(rand)
+  name = username(msg)
+  ircsock.send("PRIVMSG ".encode()+ channel + " :".encode() +name.encode() + " ".encode() + randtostring.encode() + "\n".encode())
+def username(msg):
+    msg = msg.split('!', 1)
+    msg = msg[0].split(':', 1)
+    return(msg[1])
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, port)) # Here we connect to the server using the port 6667
@@ -33,15 +44,22 @@ ircsock.send("NICK ".encode()+ botnick +"\n".encode()) # here we actually assign
 joinchan(channel) # Join the channel using the functions we previously defined
 
 while 1: # Be careful with these! it might send you to an infinite loop
-  ircmsg = ircsock.recv(2048) # receive data from the server
-  ircmsg = ircmsg.strip('\n\r'.encode()) # removing any unnecessary linebreaks.
-  print(ircmsg) # Here we print what's coming from the server
+    ircmsg = ircsock.recv(2048) # receive data from the server
+    ircmsg = ircmsg.strip('\n\r'.encode()) # removing any unnecessary linebreaks.
+    print(ircmsg) # Here we print what's coming from the server
 
-  if ircmsg.find(":Hello ".encode()+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
-    hello()
+    if ircmsg.lower().find(":Hello ".lower().encode()+ botnick.lower()) != -1: # If we can find "Hello Mybot" it will call the function hello()
+        hello(ircmsg.decode())
+    elif ircmsg.lower().find(":Hi ".lower().encode()+ botnick.lower()) != -1:
+        hello(ircmsg.decode())
+    elif ircmsg.lower().find(":Hey ".lower().encode()+ botnick.lower()) != -1:
+        hello(ircmsg.decode())
 
-  if ircmsg.find("PING :".encode()) != -1: # if the server pings us then we've got to respond!
-    ping()
+    if ircmsg.find("PING :".encode()) != -1: # if the server pings us then we've got to respond!
+        ping()
 
-  if ircmsg.find(":!help".encode()) != -1: # this looks for someone that want to know what commands exist
-    help()
+    if ircmsg.find(":!help".encode()) != -1: # this looks for someone that want to know what commands exist
+        help()
+
+    if ircmsg.find(":!roll".encode()) != -1:
+        roll(ircmsg.decode())
